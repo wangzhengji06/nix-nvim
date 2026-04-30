cc19f692757067919f1f5805161ce7510a5f1daa
b-zhengji.a.wang <b-zhengji.a.wang@rakuten.com>
2026-04-30 18:43:27 +0900

    Could this be the fix

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
+		branch = "main",
 		build = ":TSUpdate",
 		config = function()
 			require("nvim-treesitter.configs").setup({
