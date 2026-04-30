2412abb247c8205f2f5f9be0aee86031814d6de8
wangzhengji06 <wangzhengji06@gmail.com>
2026-04-30 19:34:33 +0900

    Can i get my feature back?

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,23 +1,33 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter").install({
-			"c",
-			"python",
-			"lua",
-			"vim",
-			"vimdoc",
-			"query",
-			"markdown",
-			"markdown_inline",
-		})
-		-- enable treesitter highlighting
-		vim.api.nvim_create_autocmd("FileType", {
-			callback = function(args)
-				pcall(vim.treesitter.start, args.buf)
-			end,
-		})
-	end,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").install({
+				"c",
+				"python",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+			})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local max_filesize = 100 * 1024
+					local name = vim.api.nvim_buf_get_name(args.buf)
+					local ok, stats = pcall(vim.loop.fs_stat, name)
+
+					if ok and stats and stats.size > max_filesize then
+						return
+					end
+
+					pcall(vim.treesitter.start, args.buf)
+				end,
+			})
+		end,
+	},
 }
